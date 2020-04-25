c1cbef65ea62b025c434af2cf9366f7131e369a9
knelligan <ke10nell@siena.edu>
2020-04-25 19:16:56 -0400

    added to hex tiles and player classes

--- Settlers of Catan/HexTiles.java
+++ Settlers of Catan/HexTiles.java
@@ -22,29 +22,26 @@ import javax.sound.sampled.*;
  * @author Adam Dachenhausen, Kate Nelligan, Lindsay Clark
  * @version Spring 2020
  */
-public class HexTiles
-{
+public class HexTiles extends AnimatedGraphicsObject{
     // instance variables - replace the example below with your own
     private int x;
 
     /**
      * Constructor for objects of class Tiles
      */
-    public HexTiles()
-    {
-        // initialise instance variables
-        x = 0;
+    public HexTiles(JComponent container){
+        super(container);
+    }
+    @Override
+    public void paint(Graphics g){
+
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    @Override
+    public void run(){
+
     }
+
+
+
 }
