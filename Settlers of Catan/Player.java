280ea0304685dc21610965a7a113563d6667ac18
knelligan <ke10nell@siena.edu>
2020-04-26 14:34:53 -0400

    added to player to account for points

--- Settlers of Catan/Player.java
+++ Settlers of Catan/Player.java
@@ -10,13 +10,14 @@ public class Player
     // instance variables - replace the example below with your own
     protected int playerNumber;
     protected String Color;
-    
+
     // Points for game--development is development cards
     protected int cities;
     protected int settlements;
     protected int roads;
     protected int development;
-
+    protected int victoryPoints;
+    protected boolean turn = false;
     /**
      * An example of a method - replace this comment with your own
      *
