b989e34a76fd7a023d9e8bc15ed26ef54943840d
Adam Dachenhausen <ad10dach@siena.edu>
2020-04-26 12:52:22 -0400

    Completed paint method

--- Settlers of Catan/HexTiles.java
+++ Settlers of Catan/HexTiles.java
@@ -24,7 +24,7 @@ import javax.sound.sampled.*;
  */
 public class HexTiles extends AnimatedGraphicsObject{
     public static final int POINTS = 6;
-    
+
     //I found this give "nice" height and width
     //SRC: https://hexagoncalculator.apphb.com/
     public static final int SIDE_LENGTH = 17;
@@ -32,13 +32,15 @@ public class HexTiles extends AnimatedGraphicsObject{
     public static final int PERIMETER = 102;
     public static final int AREA = 750;
     public static final int VERTEX_VERTEX_LENGTH = 34;
-    
+
     public static final int X_OFFSET = 15;
 
     protected Point center;
-    
+
     protected String type;
 
+    protected Resource r;
+    
     protected Polygon p;
     /**
      * Constructor for objects of class Tiles
@@ -47,7 +49,7 @@ public class HexTiles extends AnimatedGraphicsObject{
         super(container);
         p = new Polygon();
         this.center=center;
-        
+
         //Outsource completing the polygon to make it a hexagon
         completeHex();
     }
@@ -56,27 +58,60 @@ public class HexTiles extends AnimatedGraphicsObject{
         //Add the northern most and southernmost points
         p.addPoint(center.x+SIDE_LENGTH,center.y);
         p.addPoint(center.x-SIDE_LENGTH,center.y);
-        
+
         //Add right side points
         p.addPoint(center.x+X_OFFSET,center.y+8);
         p.addPoint(center.x+X_OFFSET,center.y-9);
-        
+
         //Add left side points
         p.addPoint(center.x-X_OFFSET,center.y+9);
         p.addPoint(center.x-X_OFFSET,center.y-8);
     }
-    
+
     @Override
     public void paint(Graphics g){
+        //For returning g back to the original color
+        Color cur = g.getColor();
         
+        //Determine what color to make the tile based on its resource
+        switch(r){
+            case BRICKS:
+            g.setColor(new Color(203, 65, 84));
+            break;
+            
+            case WOOD:
+            g.setColor(new Color(193,154,107));
+            break;
+            
+            case ORE:
+            g.setColor(new Color(149, 148, 139));
+            break;
+            
+            case WHEAT:
+            g.setColor(new Color(245, 222, 179));
+            break;
+            
+            case WOOL:
+            g.setColor(new Color(219, 219, 219));
+            break;
+            
+            case SAND:
+            g.setColor(new Color(194, 178, 128));
+            break;
+        }
+        
+        //Draw the filled polygon
+        g.fillPolygon(p);
+        
+        //Draw the polygon outline
+        g.drawPolygon(p);
+        
+        //Set g back to the original color
+        g.setColor(cur);
     }
 
     @Override
     public void run(){
-
-    }
-
-    public void addPoints(){
 
     }
 
