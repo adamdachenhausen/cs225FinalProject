627b63a94028a274557afdef67eecf05ebe3d367
Adam Dachenhausen <ad10dach@siena.edu>
2020-04-26 16:21:29 -0400

    createBoard() complete

--- Settlers of Catan/GameBoard.java
+++ Settlers of Catan/GameBoard.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
+import java.awt.Point;
+import javax.swing.JComponent;
+import java.awt.Graphics;
 /**
  * Write a description of class GameBoard here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class GameBoard
+public class GameBoard extends AnimatedGraphicsObject
 {
     public static final Color SEA = new Color(49, 159, 181);
 
@@ -23,22 +26,28 @@ public class GameBoard
     public static final int NUM_SAND = 1;
     public static final int NUM_RESOURCES = 19;
     public static final int BOARD_WIDTH = 5;
+    public static final int OFFSET = HexTiles.SIDE_LENGTH;
 
     private JPanel panel;
-    private Container frame;
     protected HexTiles[][] board;
     private Stack<Resource> r;
-    public GameBoard(Container frame){
+    private Point cur;
+    private Point topLeft;
+    public GameBoard(JComponent container, Point topLeft){
+        super(container);
+        this.container = container;
         panel = new JPanel();
         panel.setBackground(SEA);
 
         board=new HexTiles[BOARD_WIDTH][BOARD_WIDTH];
         r = new Stack<Resource>();
         populateR();
+        this.topLeft = topLeft;
+        cur = topLeft;
     }
 
     private void populateR(){
-      
+
         //Add everything to r
         for(int i=0;i<NUM_BRICKS;i++){
             r.add(Resource.BRICKS);
@@ -69,21 +78,54 @@ public class GameBoard
     }
 
     private void createBoard(){
+
         for(int i=0; i<board.length;i++){
+            boolean shifted = false;
+            if(i>0){
+                cur = new Point(topLeft.x,cur.y+OFFSET);
+            }
             for(int j=0; j<board[0].length;j++){
-                //Corners should not be drawn
-                if((i==0 || i==4) && (j==0 || j==4)){
+                //Corners should not be drawn but cur should still be moved
+                if(i==0 && (j==0 || j==4)){
                     board[i][j] = null;
+                    cur = new Point(cur.x+OFFSET,cur.y);
+                }
+                else if(i==4 && (j==0 || j==4)){
+                    board[i][j] = null;
+                    cur = new Point(topLeft.x,cur.y);
                 }
                 //The last index of row 1, and 3 should not be drawn
                 else if(i==4 && (j==1 || j==3)){
                     board[i][j] = null;
+                    cur = new Point(topLeft.x,cur.y);
                 }
                 else{
-                    //board[i][j] = new HexTiles(panel,,r.pop());
+                    //Determine if a x_offset is needed, and only shifts once
+                    if((i==1 || i==3) && !shifted){
+                        cur.translate(OFFSET,0);
+                        shifted=true;
+                    }
+                    board[i][j] = new HexTiles(panel,cur,r.pop());
+                    cur.translate(OFFSET,0);
                 }
             }
         }
 
+    }
+
+    @Override
+    public void paint(Graphics g){
+        for(int i=0; i<board.length;i++){
+
+            for(int j=0; j<board[0].length;j++){
+                board[i][j].paint(g);
+            }
+
+        }
+    }
+
+    @Override
+    public void run(){
+        
     }
 }
