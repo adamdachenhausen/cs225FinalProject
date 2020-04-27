f77b2a42e853e64c1a294e55ba3d07c60050e98e
adamdachenhausen <ad10dach@siena.edu>
2020-04-27 18:49:44 -0400

    Initial commit

---
+++ Settlers of Catan/Token.java
@@ -0,0 +1,11 @@
+
+/**
+ * Values of the tokens that determine if a player gets resources, based on dice roll
+ *
+ * @author Kate Nelligan, Lindsay Clark, Adam Dachenhausen
+ * @version Spring 2020
+ */
+public enum Token
+{
+    TWO,THREE,FOUR,FIVE,SIX,EIGHT,NINE,TEN,ELEVEN,TWELVE
+}
