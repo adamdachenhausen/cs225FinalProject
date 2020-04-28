4a2de6d04b98450f489130f9d6d755d7067731a9
adamdachenhausen <ad10dach@siena.edu>
2020-04-27 21:45:55 -0400

    Initial Commit

---
+++ Settlers of Catan/Sea.java
@@ -0,0 +1,24 @@
+import java.awt.Polygon;
+import java.awt.Point;
+/**
+ * Represents the blue hexagon that holds the many smaller hexagons
+ *
+ * @author Kate Nelligan, Lindsay Clark, Adam Dachenhausen
+ * @version Spring 2020
+ */
+public class Sea
+{
+    //This is the polygon that constructs the hexagon sea
+    public static final int SEA_SIDE_LENGTH = HexTiles.SIDE_LENGTH * GameBoard.BOARD_WIDTH;
+    public static Polygon createSea(Point topLeft){
+        Polygon p = new Polygon();
+        //Create the polygon "sea"
+        p.addPoint(topLeft.x,topLeft.y);
+        p.addPoint(topLeft.x+SEA_SIDE_LENGTH,topLeft.y);
+        //
+        //
+        //
+        //
+        return p;
+    }
+}
