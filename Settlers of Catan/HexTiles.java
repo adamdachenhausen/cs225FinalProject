116af1e0756f18e06d684ed00906d224383fe80a
Adam Dachenhausen <ad10dach@siena.edu>
2020-04-28 10:57:00 -0400

    Added second constructor

--- Settlers of Catan/HexTiles.java
+++ Settlers of Catan/HexTiles.java
@@ -37,7 +37,7 @@ public class HexTiles extends AnimatedGraphicsObject{
     public static final int X_OFFSET = 15*MULTIPLIER;
     public static final int Y_OFFSET1 = 8*MULTIPLIER;
     public static final int Y_OFFSET2 = 9*MULTIPLIER;
-    
+
     protected Point center;
 
     protected String type;
@@ -53,6 +53,21 @@ public class HexTiles extends AnimatedGraphicsObject{
         p = new Polygon();
         this.center=center;
         this.r = r;
+
+        //Outsource completing the polygon to make it a hexagon
+        completeHex();
+    }
+
+    /**
+     * Constructor for objects of class Tiles, but with added translate center functionality
+     */
+    public HexTiles(JComponent container, Point center, Resource r,Token t,int dx, int dy){
+        super(container);
+        p = new Polygon();
+        //this.center=center;
+        this.r = r;
+
+        this.center = new Point(center.x+dx,center.y+dy);
 
         //Outsource completing the polygon to make it a hexagon
         completeHex();
@@ -77,46 +92,52 @@ public class HexTiles extends AnimatedGraphicsObject{
         p.addPoint(center.x-X_OFFSET,center.y-Y_OFFSET1);
         p.addPoint(center.x-X_OFFSET,center.y+Y_OFFSET2);
     }
+
     @Override
     public void paint(Graphics g){
-        //For returning g back to the original color
-        Color cur = g.getColor();
-
-        //Determine what color to make the tile based on its resource
-        switch(r){
-            case BRICKS:
-            g.setColor(new Color(203, 65, 84));
-            break;
-
-            case WOOD:
-            g.setColor(new Color(34, 139, 34));
-            break;
-
-            case ORE:
-            g.setColor(new Color(149, 148, 139));
-            break;
-
-            case WHEAT:
-            g.setColor(new Color(245, 222, 179));
-            break;
-
-            case WOOL:
-            g.setColor(new Color(86, 125, 70));
-            break;
-
-            case SAND:
-            g.setColor(new Color(194, 178, 128));
-            break;
+        if(this == null){
+            //do nothing
         }
+        else{
+            //For returning g back to the original color
+            Color cur = g.getColor();
 
-        //Draw the filled polygon
-        g.fillPolygon(p);
+            //Determine what color to make the tile based on its resource
+            switch(r){
+                case BRICKS:
+                g.setColor(new Color(203, 65, 84));
+                break;
 
-        //Draw the polygon outline
-        g.drawPolygon(p);
+                case WOOD:
+                g.setColor(new Color(34, 139, 34));
+                break;
 
-        //Set g back to the original color
-        g.setColor(cur);
+                case ORE:
+                g.setColor(new Color(149, 148, 139));
+                break;
+
+                case WHEAT:
+                g.setColor(new Color(245, 222, 179));
+                break;
+
+                case WOOL:
+                g.setColor(new Color(86, 125, 70));
+                break;
+
+                case SAND:
+                g.setColor(new Color(194, 178, 128));
+                break;
+            }
+
+            //Draw the filled polygon
+            g.fillPolygon(p);
+
+            //Draw the polygon outline
+            g.drawPolygon(p);
+
+            //Set g back to the original color
+            g.setColor(cur);
+        }
     }
 
     @Override
