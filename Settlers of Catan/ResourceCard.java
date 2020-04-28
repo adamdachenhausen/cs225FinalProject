0c5eb0e69a0dde909a0be4d59abb8b6fbb3722b4
knelligan <ke10nell@siena.edu>
2020-04-28 19:25:30 -0400

    added to resource card and deck

--- Settlers of Catan/ResourceCard.java
+++ Settlers of Catan/ResourceCard.java
@@ -29,70 +29,79 @@ public class ResourceCard extends AnimatedGraphicsObject implements ImageObserve
     private static Image ore;
     private static Image lumber;
     private static Image wool;
+
+    private Resource cardType;
     /**
      * Constructor for objects of class ResourceCards
      */
-    public ResourceCard(JComponent container){
+    public ResourceCard(JComponent container, Resource r){
         super(container);
+        cardType = r;
     }
 
     public void showCard(){
         visible = true;
     }
 
+    public Resource getCardType(){
+        return cardType;
+    }
+    
+    public void getColor(Graphics g){
+                    //Determine what color to make the tile based on its resource
+            switch(cardType){
+                case BRICKS:
+                g.setColor(new Color(203, 65, 84));
+                break;
+
+                case WOOD:
+                g.setColor(new Color(34, 139, 34));
+                break;
+
+                case ORE:
+                g.setColor(new Color(149, 148, 139));
+                break;
+
+                case WHEAT:
+                g.setColor(new Color(245, 222, 179));
+                break;
+
+                case WOOL:
+                g.setColor(new Color(86, 125, 70));
+                break;
+
+                case SAND:
+                g.setColor(new Color(194, 178, 128));
+                break;
+            }
+    }
     @Override
     public void paint(Graphics g){
-        // //draw colored rectangle
 
-        // //paint image of card type icon
-        // if(!done){
-        // //draw image of explosion
-        // if(value == 1){
-        // g.drawImage(dice1, upperLeft.x , upperLeft.y, this);
-        // }else if(value == 2){
-        // g.drawImage(dice2, upperLeft.x , upperLeft.y, this);
-        // }else if(value == 3){
-        // g.drawImage(dice3, upperLeft.x , upperLeft.y, this);
-        // }else if(value == 4){
-        // g.drawImage(dice4, upperLeft.x , upperLeft.y, this);
-        // }else if(value == 5){
-        // g.drawImage(dice5, upperLeft.x , upperLeft.y, this);
-        // }else{
-        // //If alien
-        // g.drawImage(dice6, upperLeft.x , upperLeft.y, this);
-        // }
-        // }
-        // //paint text that describes card type
-    }
-
-    /** Populates the r stack with exact number of each development card
-     *  Then shuffles the stack, so when items are popped, they are random
-     */
-    public static Stack populateR(){
-        Stack<Resource> r = new Stack<Resource>();
-
-        //Add everything to r
-        for(int i=0;i<LUMBER;i++){
-            r.add(Resource.BRICKS);
+        if(visible){
+            // //draw colored rectangle
+            getColor(g);
+            
+            // //paint image of card type icon
+            // if(!done){
+            // //draw image of explosion
+            // if(value == 1){
+            // g.drawImage(dice1, upperLeft.x , upperLeft.y, this);
+            // }else if(value == 2){
+            // g.drawImage(dice2, upperLeft.x , upperLeft.y, this);
+            // }else if(value == 3){
+            // g.drawImage(dice3, upperLeft.x , upperLeft.y, this);
+            // }else if(value == 4){
+            // g.drawImage(dice4, upperLeft.x , upperLeft.y, this);
+            // }else if(value == 5){
+            // g.drawImage(dice5, upperLeft.x , upperLeft.y, this);
+            // }else{
+            // //If alien
+            // g.drawImage(dice6, upperLeft.x , upperLeft.y, this);
+            // }
+            // }
+            // //paint text that describes card type
         }
-
-        for(int i=0;i<WOOL;i++){
-            r.add(Resource.WOOD);
-        }
-
-        for(int i=0;i<GRAIN;i++){
-            r.add(Resource.WHEAT);
-        }
-
-        for(int i=0;i<BRICK;i++){
-            r.add(Resource.WOOL);
-        }
-
-        for(int i=0;i<ORE;i++){
-            r.add(Resource.WOOL);
-        }
-
-        return r;
     }
 
     public boolean imageUpdate(Image img, int infoflags, int x, int y,
--- Settlers of Catan/ResourceDeck.java
+++ Settlers of Catan/ResourceDeck.java
@@ -10,32 +10,60 @@ import java.util.Random;
 /**
  * Write a description of class ResourceDeck here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Kate Nelligan, Lindsay Clark, Adam Dachenhausen
+ * @version Spring 2020
  */
-public class ResourceDeck
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
+public class ResourceDeck extends AnimatedGraphicsObject{
+    ArrayList<ResourceCard> resourceDeck;
+    public static final int LUMBER = 19;
+    public static final int WOOL = 19;
+    public static final int GRAIN = 19;
+    public static final int BRICK = 19;
+    public static final int ORE = 19;
     /**
      * Constructor for objects of class ResourceDeck
      */
-    public ResourceDeck()
-    {
-        // initialise instance variables
-        x = 0;
+    public ResourceDeck(JComponent container){
+        super(container);
+        new ArrayList<ResourceCard>();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+    /** Populates the r stack with exact number of each development card
+     *  Then shuffles the stack, so when items are popped, they are random
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static Stack populateR(){
+
+        //Add everything to r
+        for(int i=0;i<LUMBER;i++){
+            resourceDeck.add(Resource.BRICKS);
+        }
+
+        for(int i=0;i<WOOL;i++){
+            resourceDeck.add(Resource.WOOL);
+        }
+
+        for(int i=0;i<GRAIN;i++){
+            resourceDeck.add(Resource.WHEAT);
+        }
+
+        for(int i=0;i<BRICK;i++){
+            resourceDeck.add(Resource.BRICKS);
+        }
+
+        for(int i=0;i<ORE;i++){
+            resourceDeck.add(Resource.ORE);
+        }
+        Collections.shuffle(r);
+        return r;
+    }
+
+    @Override
+    public void paint(Graphics g){
+
+    }
+
+    @Override
+    public void run(){
+
     }
 }
