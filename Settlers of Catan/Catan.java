0f9762798f5da0c75bdf8692ff3fc8e2035710b4
Adam Dachenhausen <ad10dach@siena.edu>
2020-04-29 09:11:11 -0400

    Shifed the param for gameboard

--- Settlers of Catan/Catan.java
+++ Settlers of Catan/Catan.java
@@ -387,7 +387,7 @@ public class Catan extends ThreadGraphicsController implements MouseListener, Mo
         //ADD BACK WHEN SEA IS DONE
         //sea = new Sea(panel, new Point(200, 5));
 
-        gameboard = new GameBoard(panel,new Point(300,300));
+        gameboard = new GameBoard(panel,new Point(350,350));
         gameboard.createBoard();
         gameboard.start();
         gameboard.startBoard();
