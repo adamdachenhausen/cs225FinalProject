bd020c6683306282367f305959743b6788249b40
Adam Dachenhausen <ad10dach@siena.edu>
2020-04-30 19:25:23 -0400

    Sea complete

--- Settlers of Catan/Sea.java
+++ Settlers of Catan/Sea.java
@@ -7,21 +7,26 @@ import java.awt.Point;
  * @version Spring 2020
  */
 public class Sea{
+    public static final int MULTIPLIER = 4;
+    public static final int X_OFFSET = 0;
+    public static final int Y_OFFSET = 0;
 
     //This is the polygon that constructs the hexagon sea
-    public static final int SEA_SIDE_LENGTH = HexTiles.SIDE_LENGTH * GameBoard.BOARD_WIDTH;
-    public static Polygon createSea(Point topLeft){
+    public static final int SEA_SIDE_LENGTH = 340;
+    public static final int APOTHEM = 294;
+    public static Polygon createSea(Point center){
         Polygon p = new Polygon();
         //Create the polygon "sea"
-        p.addPoint(topLeft.x,topLeft.y);
-        p.addPoint(topLeft.x+SEA_SIDE_LENGTH,topLeft.y);
-        //
-        //
-        //
-        //
+               
+        //Add points in clockwise rotation starting at due West point
+        p.addPoint(center.x-SEA_SIDE_LENGTH,center.y);
+        p.addPoint(center.x-SEA_SIDE_LENGTH/2,center.y-APOTHEM);
+        p.addPoint(center.x+SEA_SIDE_LENGTH/2,center.y-APOTHEM);
+        p.addPoint(center.x+SEA_SIDE_LENGTH,center.y);
+        p.addPoint(center.x+SEA_SIDE_LENGTH/2,center.y+APOTHEM);
+        p.addPoint(center.x-SEA_SIDE_LENGTH/2,center.y+APOTHEM);
+        
         return p;
     }
-    
 
 }
-
